72a7a717017e9ec67b69c253d68b324bd3723388
donghae <choi7gold@gmail.com>
2025-12-08 15:37:51 +0900

    테스트 오류 삭제

--- src/test/java/web/mvc/cicd02_boot/Cicd02BootApplicationTests.java
+++ src/test/java/web/mvc/cicd02_boot/Cicd02BootApplicationTests.java
@@ -8,7 +8,7 @@ class Cicd02BootApplicationTests {
 
     @Test
     void contextLoads() {
-        throw new RuntimeException("예외발생...");
+        //throw new RuntimeException("예외발생...");
     }
 
 }
